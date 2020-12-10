8bc8d7b7ed22eb8b535af00cece9bf25b1563e8b
Kara Martin <72467732+KaraMartin@users.noreply.github.com>
2020-12-10 11:01:05 -0500

    Appearance, functionality

--- djangoProjectFinal/urls.py
+++ djangoProjectFinal/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
     path('dmscreen/', include('DMScreen.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='DMScreen/index.html'), name='home'),
 ]
