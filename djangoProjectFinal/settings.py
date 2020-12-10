ccdd101920762f2f6b9e2deaf80c67463ca54dd9
Kara Martin <72467732+KaraMartin@users.noreply.github.com>
2020-12-10 12:21:59 -0500

    deploy (remove staticfiles in settings)

--- djangoProjectFinal/settings.py
+++ djangoProjectFinal/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ["static"]
+STATIC_ROOT = BASE_DIR / 'static'
+#STATICFILES_DIRS = ["static"]
 
 LOGIN_REDIRECT_URL = '/dmscreen'
 LOGOUT_REDIRECT_URL = '/'
